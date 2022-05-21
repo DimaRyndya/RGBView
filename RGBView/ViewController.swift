8319ad5ec073213a583a04bef22c7c57ab6a44ca
DimaRyndya <dmitriy.ryndya@gmail.com>
2022-05-21 17:33:00 +0300

    Initial Commit. Added Launchscreen label and Mainscreen View and tree labels.

---
+++ RGBView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RGBView
+//
+//  Created by Dmitriy Ryndya on 16.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
