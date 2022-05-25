3a5117f41a2ed93fb17b248c230b22028561e5e2
DimaRyndya <dmitriy.ryndya@gmail.com>
2022-05-25 18:57:29 +0300

    Connected Labeles to changing Sliders value

--- RGBView/ViewController.swift
+++ RGBView/ViewController.swift
@@ -8,12 +8,66 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var valueRedField: UILabel!
+    @IBOutlet var valueGreenField: UILabel!
+    @IBOutlet var valueBlueField: UILabel!
+    @IBOutlet var sliderForRedColor: UISlider!
+    @IBOutlet var sliderForGreenColor: UISlider!
+    @IBOutlet var sliderForBlueColor: UISlider!
+    @IBOutlet var mainColorScreen: UIView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //MARK: Main Color Field
+        mainColorScreen.backgroundColor = .white
+        
+        
+        // MARK: Labales
+        valueRedField.text = "1"
+        valueGreenField.text = "1"
+        valueBlueField.text = "1"
+        valueRedField.font = valueRedField.font.withSize(25)
+        valueGreenField.font = valueGreenField.font.withSize(25)
+        valueBlueField.font = valueBlueField.font.withSize(25)
+        valueRedField.textAlignment = .center
+        valueGreenField.textAlignment = .center
+        valueBlueField.textAlignment = .center
+        
+        
+       // MARK: Sliders
+        sliderForRedColor.value = 1
+        sliderForGreenColor.value = 1
+        sliderForBlueColor.value = 1
+        sliderForRedColor.minimumValue = 1
+        sliderForGreenColor.minimumValue = 1
+        sliderForBlueColor.minimumValue = 1
+        sliderForRedColor.maximumValue = 255
+        sliderForGreenColor.maximumValue = 255
+        sliderForBlueColor.maximumValue = 255
+        sliderForRedColor.minimumTrackTintColor = .red
+        sliderForGreenColor.minimumTrackTintColor = .green
+        sliderForBlueColor.minimumTrackTintColor = .blue
+        sliderForRedColor.thumbTintColor = UIColor(red: 255/255, green: 0/255, blue: 0/255, alpha: 1)
+        sliderForGreenColor.thumbTintColor = UIColor(red: 0/255, green: 255/255, blue: 0/255, alpha: 1)
+        sliderForBlueColor.thumbTintColor = UIColor(red: 0/255, green: 0/255, blue: 255/255, alpha: 1)
+        
+        
     }
 
-
+    @IBAction func changeValueForRed() {
+        valueRedField.text = String(Int(sliderForRedColor.value))
+    }
+    
+    
+    @IBAction func changeValueForGreen() {
+        valueGreenField.text = String(Int(sliderForGreenColor.value))
+    }
+    
+    
+    @IBAction func changeValueForBlue() {
+        valueBlueField.text = String(Int(sliderForBlueColor.value))
+    }
 }
 
